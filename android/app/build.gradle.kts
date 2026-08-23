f93d0b39d993123ba4cc762e380378abb27ad893
sxicou <sxicou@gmail.com>
2026-08-23 11:06:31 -0400

    Update Android NDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ val keystoreProperties = Properties().apply { // 同样添加了导入
 android {
     namespace = "com.mystyle.bilibilimusic"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
